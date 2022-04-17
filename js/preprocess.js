3bccf324a941297baaa3889278479275dd926a7d
Kazutaka Matsuda <kztk@ecei.tohoku.ac.jp>
2022-04-17 16:47:34 +0900

    generated from d7d55249fee67c19a363ddfebfc2982d9fe8e909

--- js/preprocess.js
+++ js/preprocess.js
@@ -61,7 +61,7 @@ function addModalImageViewPane() {
 
 function addTouchEventHadnlingToDD() {
     const dds = document.querySelectorAll('.dd');
-
+    // FIXME: Just adding 'touchstart' would be enough for iPad 
     dds.forEach( elem => {
         elem.addEventListener('touchstart', function(event) {
             if ( elem.classList.contains('touched') ) {
@@ -77,10 +77,82 @@ function addTouchEventHadnlingToDD() {
     });
 }
 
+function addCopyButtons() {
+    /*
+      This relies on the fact that pandoc generates
+
+      <div class="sourceCode"><pre class="sourceCode langName"><code class="sourceCode langName">...</code></pre></div>
+      
+      for code blocks.       
+
+     */    
+    const codeBlocks = document.querySelectorAll('pre.sourceCode'); 
+
+    
+    /*
+      <pre class="sourceCode">...</pre>
+
+      -->
+
+      <pre class="sourceCode">...
+        <div class="code-control"><span class="copyButton"></span></div>
+      </pre>      
+     */
+
+    codeBlocks.forEach (elem => {
+        // const cs = elem.querySelectorAll('code');
+        
+        // if ( cs.length != 1 ) { return true; }
+        // const code = cs[0]; 
+
+        const div = document.createElement('div');
+        div.classList.add('code-control');
+        const copyButton = document.createElement("span");
+        copyButton.classList.add('copy-button');        
+
+        const k = function() {
+            navigator.clipboard.writeText( elem.innerText ).then( function () {                        
+                // copyButton.classList.add('copied');                        
+                // setTimeout(function () {
+                //     copyButton.classList.remove('copied');
+                // }, 3000); 
+            }).catch( function(error) {
+                try {
+                    console.log("fall back to execCommand");
+                    elem.select();
+                    document.execCommand("copy");
+                }
+                catch (err) {
+                    copyButton.classList.add('copy-failed');
+                    setTimeout(function () {
+                        copyButton.classList.remove('copy-failed');
+                    }, 3000);                
+                }         
+            });
+        };
+
+        copyButton.addEventListener('click', function(e) {
+            navigator.permissions.query({name: "clipboard-write"}).then(result => {
+                if (result.state == "granted" || result.state == "prompt") {
+                    k(); 
+                }
+            }).catch( function(error) {
+                // try without permission
+                k();
+            });
+        });        
+
+        div.appendChild( copyButton ); 
+        elem.prepend( div ); 
+    });
+}
+
+
 window.onload = function () {
     addClassToCurrentPageLinks();
     addModalImageViewPane();
     addTouchEventHadnlingToDD();
+    addCopyButtons();
 };
 
 
